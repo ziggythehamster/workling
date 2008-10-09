07ca9b4f1de8a544e5c53025a00130c9ce924b65
Douglas F Shearer <dougal.s@gmail.com>
2008-10-09 15:58:38 +0100

    Fixed load/require of class in starling client test.

--- test/starling_client_test.rb
+++ test/starling_client_test.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 context "The starling client" do
+  
   specify "should load it's config as well as any given MemCache options from RAILS_ENV/config/starling.yml" do
     YAML.expects(:load).returns({ "test" => { "listens_on" => "localhost:12345", "memcache_options" => { "namespace" => "myapp_development" } } })
     
@@ -12,11 +13,12 @@ context "The starling client" do
   end
 
   specify "should be able to connect to multiple Starling instances" do
-    load 'workling/starling/client.rb'
-    YAML.expects(:load_file).returns({ "test" => { "listens_on" => "localhost:12345,127.0.0.1:54321", "memcache_options" => { "namespace" => "myapp_development" } } })
+    require 'workling/starling/client'
+    
+    # This mock is being overridden by the one in the specification above.
+    YAML.expects(:load).returns({ "test" => { "listens_on" => "localhost:12345,127.0.0.1:54321", "memcache_options" => { "namespace" => "myapp_development" } } })
     
     client = Workling::Starling::Client.new
-    # Believe this is failing because line 15 is not having the desired effect.
     client.starling_urls.should.equal ["localhost:12345", "127.0.0.1:54321"]
     client.connection.servers.first.host.should == "localhost"
     client.connection.servers.first.port.should == 12345    
